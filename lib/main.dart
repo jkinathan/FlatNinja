07ec026d6f7124c7c735e3d70581c1b43763491e
jkinathan <jkinathan@gmail.com>
2021-02-05 17:17:00 +0300

    added buttons and icons

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,29 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.purpleAccent[400],
       ),
       body: Center(
-        child: Image.asset('assets/foodie.jpg'),//Image.network('');
+        child: IconButton(
+          onPressed: () {
+              print("Fire email");
+          },
+          icon: Icon(
+            Icons.alternate_email,
+            color: Colors.purple,
+          ),
+        ),
+        // RaisedButton.icon(
+        //   onPressed: () {
+        //     print('You just clicked me');
+        //   },
+        //   icon: Icon(
+        //     Icons.email,
+        //   ),
+        //   label: Text(
+        //       'Mail me',
+        //       style: TextStyle(fontSize: 40.0,fontFamily: 'IndieFlower'),
+        //   ),
+        //   color: Colors.orange,
+        // ),
+        // Image.asset('assets/foodie.jpg'),//Image.network('');
         // Image(
         //   // image: NetworkImage('https://images.unsplash.com/photo-1550792173-5220eacae50e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80'),
         //   // image: AssetImage('assets/foodie.jpg'),
