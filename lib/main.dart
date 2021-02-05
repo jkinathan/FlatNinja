25429de91cc3b46405222d96b0a9c380401c5e0f
jkinathan <jkinathan@gmail.com>
2021-02-05 12:46:27 +0300

    fontstyles and scaffold layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+// import 'dart:html';
 
 import 'package:flutter/material.dart';
 
@@ -9,6 +9,24 @@ void main() {
       appBar: AppBar(
         title: Text("Hello to my App bar"),
         centerTitle: true,
+        backgroundColor: Colors.purpleAccent[400],
+      ),
+      body: Center(
+        child: Text(
+            "Hello Ninjaz",
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.orange[600],
+            fontFamily: 'IndieFlower' //according to the name saved in pubspec
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text("Click"),
+        backgroundColor: Colors.purpleAccent[400],
       ),
     ),
   ));
