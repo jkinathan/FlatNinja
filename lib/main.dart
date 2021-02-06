74594c590b35343ba7acac3c9065afbc6be0a0f4
jkinathan <jkinobe@gmail.com>
2021-02-06 10:08:26 +0300

    compeleted NinjaID with stateless

--- lib/main.dart
+++ lib/main.dart
@@ -12,82 +12,83 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: Text("Hello to my App bar"),
+        title: Text("Ninja ID CARD"),
         centerTitle: true,
         backgroundColor: Colors.purpleAccent[400],
+        elevation: 0.0,
       ),
-      body:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0,40.0,30.0,0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/ninja1.png'),
+                  radius: 50.0,
+                ),
+              ),
+              Divider(height: 60.0,color: Colors.yellowAccent),
+              Text(
+                  'NAME:',
+                style: TextStyle(
+                    color: Colors.red,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Kinobe Jordan',
+                style: TextStyle(
+                  color: Colors.yellowAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                'Current Ninja Level:',
+                style: TextStyle(
+                  color: Colors.red,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                '8',
+                style: TextStyle(
+                  color: Colors.yellowAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30.0),
               Row(
                 children: [
-                  Expanded(child: Image.asset('assets/foodie.jpg'),flex: 3,),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.pink,
-                      padding: EdgeInsets.all(30.0),
-                      child: Text('1'),
-                    ),
+                  Icon(
+                    Icons.email,
+                    color: Colors.yellowAccent,
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.yellowAccent,
-                      padding: EdgeInsets.all(30.0),
-                      child: Text('2'),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
+                  SizedBox(width: 20.0),
+                  Text(
+                      'jkinobe@gmail.com',
+                    style: TextStyle(
                       color: Colors.red,
-                      padding: EdgeInsets.all(30.0),
-                      child: Text('3'),
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
                     ),
+
                   ),
                 ],
               ),
-              Expanded(
-                child: Container(
-                  color: Colors.green,
-                  padding: EdgeInsets.all(40.0),
-                  child: Text('Two'),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.deepOrangeAccent,
-                  padding: EdgeInsets.all(30.0),
-                  child: Text('One'),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.blue,
-                  padding: EdgeInsets.all(50.0),
-                  child: Text('Three'),
-                ),
-              ),
             ],
-          ),
-      // Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //   crossAxisAlignment: CrossAxisAlignment.center,
-      //   children: [
-      //     Text('Hello World'),
-      //     FlatButton(onPressed: (){}, color: Colors.purple,child: Text('Press me'),),
-      //     Container(
-      //       color: Colors.amberAccent,
-      //       padding: EdgeInsets.all(30.0),
-      //       child: Text('Inside my Container'),
-      //     ),
-      //   ],
-      //
-      // ),
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         child: Text("Click"),
